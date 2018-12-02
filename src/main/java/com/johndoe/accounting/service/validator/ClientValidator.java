42c2d45c700c2feed96eb3c681852a2aeb35722b
Nebil <nebil@nns-macbook-pro.home>
2018-12-02 01:53:14 -0600

    Second commit with client validator added

---
+++ src/main/java/com/johndoe/accounting/service/validator/ClientValidator.java
@@ -0,0 +1,82 @@
+package com.johndoe.accounting.service.validator;
+
+import com.johndoe.accounting.service.clientType.Type;
+import com.johndoe.accounting.service.exceptions.InvalidClientInputException;
+import com.johndoe.accounting.service.request.ClientRequest;
+
+import java.time.LocalDate;
+import java.time.Period;
+
+/**
+ * Created by nebil on 12/2/18.
+ */
+public class ClientValidator {
+
+    private ClientRequest clientRequest;
+    StringBuilder exceptionMessages = new StringBuilder();
+
+    public ClientValidator(ClientRequest clientRequest){
+        this.clientRequest = clientRequest;
+    }
+
+    private void validateFirstName()throws InvalidClientInputException{
+        try {
+            if (!clientRequest.getFirstName().matches("[a-zA-Z]+") && clientRequest.getType().equals("Individual")) {
+                throw new InvalidClientInputException("Invalid input for first name");
+            }
+        }
+        catch (InvalidClientInputException exception){
+            exceptionMessages.append(exception.getMessage())
+            .append(System.getProperty("line.separator"));
+        }
+    }
+
+    private void validateLastName()throws InvalidClientInputException{
+        try {
+            if (!clientRequest.getLastName().matches("[a-zA-Z]+") && clientRequest.getType().equals("Individual")) {
+                throw new InvalidClientInputException("Invalid input for last name");
+            }
+        }
+        catch (InvalidClientInputException exception){
+            exceptionMessages.append(exception.getMessage())
+            .append(System.getProperty("line.separator"));
+        }
+    }
+
+    private void validateAge()throws InvalidClientInputException{
+
+        try {
+            Period period = Period.between(clientRequest.getBirthDate(), LocalDate.now());
+            if (clientRequest.getType().equals("Individual") && period.getYears() < 18) {
+                throw new InvalidClientInputException("Person is underage!");
+            }
+        }
+        catch (InvalidClientInputException exception){
+            exceptionMessages.append(exception.getMessage())
+            .append(System.getProperty("line.separator"));
+        }
+    }
+
+    private void validateNumberOfEmployees()throws InvalidClientInputException{
+        try {
+            if (clientRequest.getType().equals("Business") && clientRequest.getNumberOfEmployee() <= 0) {
+                throw new InvalidClientInputException("Number of employees can't be 0 or negative");
+            }
+        }
+        catch (InvalidClientInputException exception){
+            exceptionMessages.append(exception.getMessage())
+            .append(System.getProperty("line.separator"));
+        }
+    }
+
+    public void validate()throws InvalidClientInputException{
+        validateFirstName();
+        validateLastName();
+        validateAge();
+        validateNumberOfEmployees();
+        if(exceptionMessages.length()>0){
+            throw new InvalidClientInputException(exceptionMessages.toString().trim());
+        }
+    }
+    
+}
